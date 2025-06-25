cb850af6e8d03c217578bf4fd325bfef2e3c2817
Yana Ponomarenko <yponomar@softserveinc.com>
2025-06-25 18:28:03 +0200

    extended tailwind styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Roboto"],
+        body: ["Inter"],
+      },
+      colors: {
+        primary: "#3B82F6",
+        success: "#22C55E",
+        warning: "#F97316",
+        error: "#EF4444",
+        gray: {
+          100: "#F3F4F6",
+          200: "#E5E7EB",
+          500: "#6B7280",
+          900: "#111827",
+        },
+        background: "#F9FAFB",
+      },
+    },
   },
   plugins: [],
-}
+};
