29e186010ceb75a71ebfd6c050a89f183403e479
Yana Ponomarenko <yponomar@softserveinc.com>
2025-06-27 17:01:46 +0200

    delete unnecessary box in filter area, fix clear filter button, use global colors and fonts

--- frontend/src/shared/components/ui/SearchFilters/SearchFilters.styles.ts
+++ frontend/src/shared/components/ui/SearchFilters/SearchFilters.styles.ts
@@ -1,19 +1,18 @@
 import styled from '@emotion/styled';
 import { Box, TextField, Button, Switch } from '@mui/material';
+import { fonts } from '@/shared/constants/fonts'
 
 export const SearchFiltersContainer = styled(Box)`
   display: flex;
   align-items: center;
-  gap: 16px;
-  background-color: #FFFFFF;
-  padding: 16px;
-  box-shadow: 0px 0px 2px 0px rgba(65, 64, 69, 0.20);
+  gap: 20px;
+  background-color: white;
 `;
 
 export const SearchField = styled(TextField)`
   min-width: 300px;
   .MuiOutlinedInput-root {
-    border-radius: 4px;
+    border-radius: 6px;
   }
 `;
 
@@ -21,14 +20,27 @@ export const FilterSelect = styled(TextField)`
   min-width: 120px;
 `;
 
-export const ClearFiltersButton = styled(Button)`
-  white-space: nowrap;
-`;
+export const ClearFiltersButton = styled(Button)({
+  backgroundColor: '#EFF0F3',    
+  color: 'black',              
+  borderRadius: '6px',
+  padding: '8px 16px',
+  fontFamily: fonts.heading,
+  fontSize: '14px',
+  fontWeight: 500,
+  lineHeight: '20px',
+  textTransform: 'none',
+  whiteSpace: 'nowrap',
+
+  '&:hover': {
+    backgroundColor: '#E9EBEE',
+  },
+});
+
 
 export const AlertsContainer = styled(Box)`
   display: flex;
   align-items: center;
-  gap: 8px;
 `;
 
 export const AlertsSwitch = styled(Switch)`
